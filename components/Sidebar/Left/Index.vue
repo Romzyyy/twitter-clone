b185bd6bdabdabeb8119ead56eb21c3099bf4239
romzyyy <fijay0117@gmail.com>
2024-03-01 18:15:48 +0700

    first commit

---
+++ components/Sidebar/Left/Index.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="h-screen flex flex-col">
+        <div
+            class="p-2 my-2 hover:bg-blue-50 hover:rounded-full w-min dark:hover:bg-white/20"
+            :class="transition">
+            <nuxt-link to="">
+                <div class="w-8 h-8">
+                    <LogoTwitter />
+                </div>
+            </nuxt-link>
+        </div>
+
+        <div class="mt-2 space-y-3">
+            <SidebarLeftTab active>
+                <template v-slot:icon>
+                    <HomeIcon />
+                </template>
+                <template v-slot:name> Home </template>
+            </SidebarLeftTab>
+            <SidebarLeftTab>
+                <template v-slot:icon>
+                    <HashtagIcon />
+                </template>
+                <template v-slot:name> Explore </template>
+            </SidebarLeftTab>
+            <SidebarLeftTab>
+                <template v-slot:icon>
+                    <BellIcon />
+                </template>
+                <template v-slot:name> Notifications </template>
+            </SidebarLeftTab>
+            <SidebarLeftTab>
+                <template v-slot:icon>
+                    <InboxIcon />
+                </template>
+                <template v-slot:name> Messages </template>
+            </SidebarLeftTab>
+            <SidebarLeftTab>
+                <template v-slot:icon>
+                    <BookmarkIcon />
+                </template>
+                <template v-slot:name> Bookmarks </template>
+            </SidebarLeftTab>
+            <SidebarLeftTab>
+                <template v-slot:icon>
+                    <DocumentTextIcon />
+                </template>
+                <template v-slot:name> List </template>
+            </SidebarLeftTab>
+            <SidebarLeftTab>
+                <template v-slot:icon>
+                    <UserIcon />
+                </template>
+                <template v-slot:name> Profile </template>
+            </SidebarLeftTab>
+            <SidebarLeftTab>
+                <template v-slot:icon>
+                    <SparklesIcon />
+                </template>
+                <template v-slot:name> More </template>
+            </SidebarLeftTab>
+        </div>
+    </div>
+</template>
+<script setup>
+import { HomeIcon } from '@heroicons/vue/24/solid'
+import {
+    HashtagIcon,
+    BellIcon,
+    InboxIcon,
+    BookmarkIcon,
+    DocumentTextIcon,
+    UserIcon,
+    SparklesIcon,
+} from '@heroicons/vue/24/outline'
+const { defaultTransition } = useTailwindConfig()
+</script>
